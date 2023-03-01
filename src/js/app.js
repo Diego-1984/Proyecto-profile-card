fc34d319e057a699e9668301c6032c7f0671c312
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-01 19:20:53 +0000

    Null arreglado falta arreglar links

--- src/js/app.js
+++ src/js/app.js
@@ -13,10 +13,10 @@ function render(variables = {}) {
                                      
           
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademyes"><i class="fab fa-instagram"></i></a></li>
           </ul>
 
           
@@ -36,11 +36,11 @@ window.onload = function() {
     github: "example@github.com",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Name",
+    lastname: "Last Name",
+    role: "Role",
+    country: "Country",
+    city: "City"
   };
   render(window.variables);
 
