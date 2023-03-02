1911d115ab899494fcd2df15a0c5886448bad99f
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-02 11:19:35 +0000

    Links twiter y github ok (instagram y linkedin no ok)

--- src/js/app.js
+++ src/js/app.js
@@ -13,10 +13,10 @@ function render(variables = {}) {
                                      
           
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademyes"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/"${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
 
           
@@ -32,10 +32,10 @@ window.onload = function() {
 
     socialMediaPosition: "position-left",
 
-    twitter: null,
-    github: "example@github.com",
-    linkedin: null,
-    instagram: null,
+    twitter: "4geeksacademy",
+    github: "4geeksacademy",
+    linkedin: "school/4geeksacademyes",
+    instagram: "4geeksacademyes",
     name: "Name",
     lastname: "Last Name",
     role: "Role",
